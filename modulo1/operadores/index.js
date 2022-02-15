7c675504e0436312193124fad71aa8c6e0c5e85e
Gabriel Menezes Souza <gah.souza1996gm@gmail.com>
2022-02-15 16:48:56 -0300

    foi adicionado a pasta Operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,73 @@
+/*
+ a. false      
+ b. false
+ c. true
+ d. boolean
+
+ 2
+    os numeros concatenados.
+ 3 
+    let primeiroNumero =Number (prompt("Digite um numero!"))
+    let segundoNumero =Number(   prompt("Digite outro numero!"))
+    const soma = primeiroNumero + segundoNumero
+    console.log(soma)
+    
+ 
+
+
+*/
+
+
+/*
+
+// 1 //
+
+    let idadeUsuario =Number (prompt("Digite a idade do usuario"))
+    let idadeAmig =Number(   prompt("Digite a idade do amigo(a)"))
+    console.log("Sua idade é do que do seu amigo(a)",idadeAmig<idadeUsuario);
+    console.log(idadeUsuario-idadeAmig)
+
+// 2 //
+
+    let numeroPar =Number (prompt("Digite um numero par"))
+    console.log(numeroPar%2);
+// numeros pares o resto da divisão é 0 
+
+// 3 //
+    let idadeAnos =Number (prompt("Digite a idade em anos"))
+
+    console.log("idade em meses : "+idadeAnos*12+" , idade em dias : "+idadeAnos*365.25 +"idade em horas : "+idadeAnos*8766);
+
+// 4 // 
+    let numero1 =Number (prompt("Digite um numero "))
+    let numero2 =Number (prompt("Digite outro numero "))
+    console.log("O primeiro numero é maior que o segundo?",numero1>numero2);
+    console.log("O primeiro numero é igual ao segundo?",numero1==numero2);
+    console.log("O primeiro numero é divisível pelo segundo? ",numero1%numero2==0);
+    console.log("O segundo numero é divisível pelo primeiro?",numero2%numero1==0);
+
+//// DESAFIOS ////
+
+//1//
+
+let temperaturaK 
+let temperaturaC 
+let temperaturaF  
+
+temperaturaK = (77-32)*5/9+273.15;
+
+console.log("temperatura e, Kelvin "+temperaturaK+"K°");
+
+temperaturaC = 80*9/5+32; 
+console.log("temperatura em F° : "+temperaturaC + "C°")
+temperaturaF = 30*9/5+32;
+temperaturaK = (temperaturaF -32)*5/9+273.15;
+console.log("temperatura em F :",temperaturaF + "F° , temperara em K :",temperaturaK +"K°");
+
+
+temperaturaC =Number (prompt("Digite uma temperatura "))
+   
+temperaturaF = temperaturaC*9/5+32;
+temperaturaK = (temperaturaF -32)*5/9+273.15;
+console.log("temperatura em F :",temperaturaF + "F° , temperara em K :",temperaturaK +"K°");
+/*/
