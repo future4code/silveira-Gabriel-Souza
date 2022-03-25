a679a6d0336428d6c0f4b475e13daad394e23663
Gabriel Menezes Souza <gah.souza1996gm@gmail.com>
2022-03-25 00:19:05 -0300

    foram criados obejtos e array , e styled-components

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -5,30 +5,101 @@ import Post from './components/Post/Post';
 const MainContainer = styled.div`
   display: flex;
   justify-content: center;
-  flex-direction: column;
+  flex-direction: column-reverse;
   align-items: center;
+`
+const insertContainer = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding-left: 1000px;
+  
 `
 
 class App extends React.Component {
+  state = {
+    posts:[
+      {
+          nomeUsuario:'paulinha',
+          fotoUsuario:'https://picsum.photos/50/50',
+          fotoPost:'https://picsum.photos/200/153'
+      },
+      {
+          nomeUsuario:'leticia',
+          fotoUsuario:'https://picsum.photos/51/50',
+           fotoPost:'https://picsum.photos/200/120'
+      },
+        {
+          nomeUsuario:'gabriel',
+          fotoUsuario:'https://picsum.photos/52/50',
+          fotoPost:'https://picsum.photos/200/151'
+        }
+    ],
+    valorInputNomeUsuario:"",
+    valorInputFotoUsuario:"",
+    valorInputFotoPost:"",
+  };
+  adiconaPost = () => {
+    const novoPost={
+      nomeUsuario:this.state.valorInputNomeUsuario,
+      fotoUsuario:this.state.valorInputFotoUsuario,
+      fotoPost:this.state.valorInputFotoPost,
+    };
+  const novoPosts=[...this.state.posts,novoPost];
+  this.setState({posts:novoPosts});
+  this.setState({valorInputFotoPost:""});
+  this.setState({valorInputFotoUsuario:""});
+  this.setState({valorInputNomeUsuario:""});
+
+
+  };
+  onChangeInputFotoUsuario=(event)=>{
+    this.setState({valorInputFotoUsuario:event.target.value});
+
+  };
+  onChangeInputNomeUsuario=(event)=>{
+    this.setState({valorInputNomeUsuario:event.target.value});
+
+  };
+  onChangeInputFotoPost=(event)=>{
+    this.setState({valorInputFotoPost:event.target.value});
+
+  };
+
   render() {
+    let listaDePosts = this.state.posts.map((post) => { return (<Post nomeUsuario={post.nomeUsuario}
+      fotoUsuario={post.fotoUsuario}  fotoPost={post.fotoPost} />)})
     return (
-      <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
+
+      <div>
+     <insertContainer>
+      <input
+        value={this.state.valorInputNomeUsuario}
+        onChange={this.onChangeInputNomeUsuario}
+        placeholder={"Nome do Usuario"}
         />
-        <Post
-          nomeUsuario={'Gabriel '}
-          fotoUsuario={'https://picsum.photos/50/51'}
-          fotoPost={'https://picsum.photos/200/151'}
+      <input
+        value={this.state.valorInputFotoUsuario}
+        onChange={this.onChangeInputFotoUsuario}
+        placeholder={"Link foto do Usuario"}
         />
-        <Post
-          nomeUsuario={'Leticia'}
-          fotoUsuario={'https://picsum.photos/50/52'}
-          fotoPost={'https://picsum.photos/200/152'}
+        <input
+        value={this.state.valorInputFotoPost}
+        onChange={this.onChangeInputFotoPost}
+        placeholder={"Link Foto do Post"}
         />
-      </MainContainer>
+        <button onClick={this.adiconaPost}>Adicionar</button>
+
+
+     </insertContainer>
+
+     <MainContainer>
+    
+      {listaDePosts}
+      
+     </MainContainer>
+      </div>
+    
     );
   }
 }
