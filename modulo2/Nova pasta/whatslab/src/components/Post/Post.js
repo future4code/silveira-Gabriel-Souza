c59a9608c09afd3c012e379b25a2e3a256266fdf
Gabriel Menezes Souza <gah.souza1996gm@gmail.com>
2022-03-28 01:56:41 -0300

    Criado projeto Whats Lab

---
+++ modulo2/Nova pasta/whatslab/src/components/Post/Post.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { IconeComContador } from "../IconeComContador/IconeComContador";
+import iconeCoracaoBranco from "../../img/favorite-white.svg";
+import iconeCoracaoPreto from "../../img/favorite.svg";
+import iconeComentario from "../../img/comment_icon.svg";
+import { SecaoComentario } from "../SecaoComentario/SecaoComentario";
+import styled from "styled-components";
+
+const PostContainer = styled.div`
+  border: 1px solid black;
+  width: 650px;
+  margin-bottom: 10px;
+`;
+
+const PostHeader = styled.div`
+  height: 50px;
+  width: 120px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-left: 10px;
+`;
+
+
+
+
+
+
+class Post extends React.Component {
+ 
+
+  render() {
+    
+
+    
+
+    let componenteComentario;
+
+   
+
+    return (
+      <PostContainer>
+        <PostHeader>
+     
+          <p>{this.props.nomeUsuario}</p>
+          <p>{this.props.mensagemUsuario}</p>
+         
+         
+        </PostHeader>
+       
+
+
+     
+
+       
+        {componenteComentario}
+      </PostContainer>
+    );
+  }
+}
+
+export default Post;
